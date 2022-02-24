ff88c44627a9e8293a256de3a29e47cffb066d84
Sam Hadi <worker@Sams-MacBook-Pro.local>
2022-02-24 13:23:39 -0800

    remmove prefetch middleware

--- components/Doc.tsx
+++ components/Doc.tsx
@@ -1,7 +1,9 @@
+import {renderToString} from 'react-dom/server';
 
 // container to render the markdown file 
 function Doc({children}){
-
+    
+    console.log(renderToString(children))
     return <>
         <main>
             {children}
